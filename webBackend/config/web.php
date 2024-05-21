b72ede3db57a85df7cb7a262ebc07f5d928a8315
johntymuriithi <muriithijohn634@gmail.com>
2024-05-21 09:10:11 +0300

    finish the sign up form

--- webBackend/config/web.php
+++ webBackend/config/web.php
@@ -13,6 +13,13 @@ $config = [
     ],
     'components' => [
         'request' => [
+//            'class' => 'yii\web\Response',
+//            'on beforeSend' => function ($event) {
+//                $response = $event->sender;
+//                $response->headers->add('Access-Control-Allow-Origin', 'http://localhost:5173');
+//                $response->headers->add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//                $response->headers->add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//            },
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'Xr2lmhIFc7MjeeiBaksZQwL6377C99nD',
             'parsers' => [
--- webBackend/controllers/UserController.php
+++ webBackend/controllers/UserController.php
@@ -6,22 +6,23 @@ use yii\rest\ActiveController;
 use app\models\User;
 use yii\web\Response;
 use yii\web\BadRequestHttpException;
+use yii\web\ServerErrorHttpException;
 
 class UserController extends ActiveController
 {
-    public $modelClass = 'app\models\User';
+    public $modelClass = 'app\models\User'; // specifies the model this controller will use
 
-    public function actions()
+    public function actions() // modifies the default actions defined by the ActiveController class
     {
-        $actions = parent::actions();
+        $actions = parent::actions(); // gets the default actions
         unset($actions['create'], $actions['update'], $actions['delete']);
         return $actions;
     }
 
     public function actionSignup()
     {
-        Yii::$app->response->format = Response::FORMAT_JSON;
-        $params = Yii::$app->request->bodyParams;
+        Yii::$app->response->format = Response::FORMAT_JSON; // sets the response to be inform of JSON format
+        $params = Yii::$app->request->bodyParams; // retrives the request body params the params passed
 
         $requiredFields = ['first_name', 'second_name', 'email', 'phone', 'password'];
 
@@ -39,9 +40,16 @@ class UserController extends ActiveController
         $user->setPassword($params['password']);
         $user->generateAuthKey();
 
+        $availableUser = User::findOne(['email' => $params['email']]);
+        $phone = User::findOne(['phone' => $params['phone']]);
+//        var_dump($availableUser);
         if ($user->save()) {
             return ['status' => true, 'message' => 'User created successfully'];
-        } else {
+        } elseif ($availableUser != null) {
+            return ['status' => 409, 'message' => 'Email already exist ', 'user' => $user];
+        }elseif ($phone != null) {
+            return ['status' => 409, 'message' => 'phone already exist ', 'user' => $user];
+        } else{
             return ['status' => false, 'errors' => $user->errors];
         }
     }
